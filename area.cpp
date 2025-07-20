6e4443bbecda5355380c740a89dd656680d15faa
0xSaad <75455552+Saad711T@users.noreply.github.com>
2025-07-21 02:05:04 +0300

    Add files via upload

---
+++ area.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int width;
+int height;
+int main() {
+    cout << "Enter the width : " << endl;
+    cin >> width;
+    cout << "Enter the height : " << endl;
+    cin >> height;
+    cout << "The Area : " << height * width << endl;
+    return 0;
+}
